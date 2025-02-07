fca76e15c62393d374d946b583e195dd882d6603
Iixzz <lennardpaul007@gmail.com>
2025-02-07 20:30:08 +0100

    add class_Auto-Git.py

--- Auto-Git.py
+++ Auto-Git.py
@@ -7,44 +7,66 @@ from time import sleep
 
 aufgabe = int(input("push(1) oder pull(2): "))
 repository = str(input("URL des GitHub-Repository: "))
-datei = str(input("Dateiname zum hinzufügen (für alle .): "))
-message = str(input("commit Message: "))
 
+datein_liste = []
+while True:
+    datei = str(input("Gib eine Dateinnamen ein (oder . für alle oder ENTER zum Beenden): "))
+    if datei == "" :
+        break
+    datein_liste.append(datei)
+
+message = str(input("commit Message: "))
+#tag = str(input("Wollen Sie der Versin einen tag geben: (y/n)"))
 
 #Text eingeben
 def text(msg1, msg2):
     kb.write(msg1 +msg2)
     kb.press_and_release("enter")
 
-#Terminal öffnen und auf Arbeitsverzeichnis leiten
-os.system("start cmd")
-script_dir = os.path.dirname(os.path.abspath(__file__))
-sleep(0.5)
+if aufgabe == 1:
+    #Terminal öffnen und auf Arbeitsverzeichnis leiten
+    os.system("start cmd")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    sleep(0.5)
 
-text("cd /d ", script_dir)
-sleep(0.5)
+    text("cd /d ", script_dir)
+    sleep(0.5)
 
-#git initiieren
-text("git init" , "")
-sleep(0.5)
+    #git initiieren
+    text("git init" , "")
+    sleep(0.5)
 
-#repository einrichten
-text("git remote add origin ", repository)
-sleep(0.5)
+    #repository einrichten
+    text("git remote add origin ", repository)
+    sleep(0.5)
 
-#git Datein hinzufügen
-if datei == ".":
-    text("git add .", "")
-sleep(0.5)
+    #git Datein hinzufügen
+    vorhandene_datein = os.listdir(script_dir)
+    if datein_liste[0] == ".":
+        text("git add .", "")
+    else:
+        for datei in datein_liste:
+            if datei in vorhandene_datein:
+               text("git add ", datei)
+            else:
+                print(f"{datei} nicht vorhanden! Überprüfen Sie die Schreibweise.")
+                
+    sleep(0.5)
 
-#Datein commiten
-if message != "": 
-    text("git commit -m ", message)
+    #Datein commiten
+    if message != "": 
+        text("git commit -m ", message)
+    else:
+        print("Üngültige eingabe")
+        message = str(input("commit Message: "))
+    sleep(0.5)
+
+    #Datein auf GitHub pushen 
+    text("git push origin master", "")
+
+elif aufgabe == 2:
+    pass
+
 else:
-    print("Üngültige eingabe")
-    message = str(input("commit Message: "))
-sleep(0.5)
-
-#Datein auf GitHub pushen 
-text("git push origin master", "")
-
+    print("üngültige Eingabe")
+    aufgabe = int(input("push(1) oder pull(2): "))
--- test1.py
+++ test1.py
@@ -1,1 +1,24 @@
+import os
+import keyboard as kb
 
+def text(msg1, msg2):
+    kb.write(msg1 +msg2)
+    kb.press_and_release("enter")
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+datein_liste = []
+while True:
+    datei = str(input("Gib eine Dateinnamen ein (oder . für alle oder ENTER zum Beenden): "))
+    if datei == "" :
+        break
+    datein_liste.append(datei)
+vorhandene_datein = os.listdir(script_dir)
+
+if datein_liste[0] == ".":
+    text("git add .", "")
+else:
+    for datei in datein_liste:
+        if datei in vorhandene_datein:
+            text("git add ", datei)
+        else:
+            print(f"{datei} nicht vorhanden! Überprüfen Sie die Schreibweise.")
