219263839db1cea6aff10451e8136136eaa6e5db
Iixzz <lennardpaul007@gmail.com>
2025-02-11 10:02:17 +0100

    push fertig

--- push_Auto-Git.py
+++ push_Auto-Git.py
@@ -2,37 +2,9 @@ import os
 import keyboard as kb
 from time import sleep
 
-aufgabe = int(input("push(1) oder pull(2) oder mit tag(3): "))
 
 
-class push1():
-    def __init__(self):
-        self.repository_URL = str(input("URL des GitHub-Repository: "))
-        self.script_dir = str(input("Ordner zum downlowden (Dateipfad): "))
-        self.commit_message = str(input("message: "))
-        convert_commit_message = f'"{self.commit_message}"'
-
-        self.datein_liste = []
-        self.vorhandene_dateien = os.listdir(self.script_dir)
-        while True:
-            self.datei = str(input("Gib eine Dateinnamen ein (oder . für alle oder ENTER zum Beenden): "))
-            if self.datei == "" :
-                break
-            elif self.datei == ".":
-                self.datein_liste = "."
-                break
-            else:
-                if self.datei in self.vorhandene_dateien:
-                    self.datein_liste.append(self.datei)
-                else:
-                    print("datei nicht vorhanden")
-                    pass
-
-
-        os.system("start cmd")
-        sleep(1)
-
-        text("cd /d ", self.script_dir)
+"""text("cd /d ", self.script_dir)
 
         text("git init", "")
         text("git remote add origin ", self.repository_URL)
@@ -44,17 +16,16 @@ class push1():
                 text("git add", datei)
 
         text("git commit -m ", convert_commit_message)
-        text("git push origin master", "")
+        text("git push origin master", "")"""
 
 class push():
     def __init__(self):
-        os.system("start cmd")
-
-    def setVariabls(self):
-        self.repository_URL = str(input("URL des GitHub-Repository: "))
         self.script_dir = str(input("Ordner zum downlowden (Dateipfad): "))
+        self.repository_URL = str(input("URL des GitHub-Repository: "))
         self.commit_message = str(input("message: "))
-        convert_commit_message = f'"{self.commit_message}"'
+        self.convert_commit_message = f'"{self.commit_message}"'
+
+        self.doPush()
 
     def checkDatein(self):
         self.datein_liste = []
@@ -74,7 +45,27 @@ class push():
                     pass
         return self.datein_liste
     
-    
+    def doPush(self):
+        os.system("start cmd")
+        sleep(0.5)
+
+        text("cd /d ", self.script_dir)
+
+        text("git init", "")
+        text("git remote add origin ", self.repository_URL)
+
+        self.checkDatein()
+
+        if self.datein_liste == ".":
+            text("git add ", self.datein_liste)
+        else:
+            for datei in self.datein_liste:
+                text("git add ", datei)
+
+        text("git commit -m ", self.convert_commit_message)
+        text("git push origin master", "")
+
+
 
 def text(msg1, msg2):
     kb.write(msg1 +msg2)
@@ -83,7 +74,7 @@ def text(msg1, msg2):
 #Ausführung
 def main():
     git_push = push()
-    git_push.setVariabls()
+    
 
 
 if __name__ == "__main__":
