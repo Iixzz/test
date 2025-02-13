e7adc3b03480f42ebc1c82f9761f393246af76cc
Iixzz <lennardpaul007@gmail.com>
2025-02-13 13:19:12 +0100

    test

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
-# Benutzer gibt mehrere Werte ein, durch Komma getrennt
-eingabe = input("Gib mehrere Werte ein, getrennt durch Komma: ")
+datein_liste = []
+datein_eingabe = "1;2;3;4"
+"""
+try:
+    self.vorhandene_dateien = os.listdir(self.script_dir)
+except Exception as e:
+    print(f"Fehler: {e}")
+    sys.exit()
+    """
 
-# Aufteilen der Eingabe an den Kommata
-werte = eingabe.split(",")  # Hier wird die Eingabe an jedem Komma aufgeteilt
+def asd(datein_eingabe): 
+    if ";" in datein_eingabe:
+        datein_ = datein_eingabe.split(";")
+        print(datein_eingabe)
+    elif datein_eingabe == ".":
+        datein_liste = "."
 
-# Ausgabe der aufgeteilten Werte
-print("Aufgeteilte Werte:")
-for wert in werte:
-    print(wert.strip())
+"""while True:
+    if datein_eingabe in datein_liste:
+        pass
+    break    """      
+
+asd(datein_eingabe)
